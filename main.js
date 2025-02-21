beadeb28bbd2b6d0a5ded5e7e22872576d535452
alexciccarese <alex.ciccarese@gmail.com>
2025-02-21 16:02:57 +0100

    add snack 2 first part

--- main.js
+++ main.js
@@ -48,3 +48,66 @@ const lightBike = lighterBike()
 bikeTextEl.innerText = `La bici più leggera è ${lightBike.name} e pesa ${lightBike.weight} kg`
 
 
+
+
+/* Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+const club = [
+  {
+    name: 'Napoli',
+    points: 0,
+    fouls: 0
+  },
+  {
+    name: 'Milan',
+    points: 0,
+    fouls: 0
+  },
+  {
+    name: 'Inter',
+    points: 0,
+    fouls: 0
+  },
+  {
+    name: 'Juventus',
+    points: 0,
+    fouls: 0
+  },
+  {
+    name: 'Lecce',
+    points: 0,
+    fouls: 0
+  },
+  {
+    name: 'Fiorentina',
+    points: 0,
+    fouls: 0
+  },
+  {
+    name: 'Roma',
+    points: 0,
+    fouls: 0
+  },
+  {
+    name: 'Atalanta',
+    points: 0,
+    fouls: 0
+  },
+]
+
+//generiamo numeri random per i punti
+function numbRandom() {
+
+  for (let i in club) {
+    club[i].points = Math.floor(Math.random() * 100) + 1
+    club[i].fouls = Math.floor(Math.random() * 100) + 1
+  }
+}
+numbRandom()
+
+console.log(club);
